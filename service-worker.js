d4595b9aec790c45429ba555faba1139a5b8ce01
aaa <aaa>
2021-11-15 16:48:25 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4408dbc0aa7cd0b61901ae4c17b9aa45.js"
+  "/precache-manifest.0ddc03799af9cefdb720b60f68c074e6.js"
 );
 
 workbox.clientsClaim();
