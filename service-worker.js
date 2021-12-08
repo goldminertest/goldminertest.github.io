c46147ae7d8110c691a710a2f53aeb08d4eacfa1
divor <divor>
2021-12-08 14:45:20 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.67f17e0fca1d9dd2a64aeac130b2a21b.js"
+  "/precache-manifest.8ab1d5ab14afc79836356e1dd86b1aae.js"
 );
 
 workbox.clientsClaim();
