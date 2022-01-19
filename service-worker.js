e8bef9b1f0316067e93046b4cd126329333aab2e
divor <divor>
2022-01-19 21:37:45 +0800

    update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6423353ac373c9d16ca87e378edb9c04.js"
+  "/precache-manifest.9faf3371a995173509f8c5b5a66b7da1.js"
 );
 
 workbox.clientsClaim();
